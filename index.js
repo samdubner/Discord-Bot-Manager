6d095e47c8edb1399f708cd91cfc71e560ccc3e6
SamuelDub <dubnersamuel@yahoo.com>
2017-07-15 16:30:05 -0700

    test updater/start check for update

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const dialog = electron.dialog
+const Menu = electron.Menu
 
 //uses github repo as update server
 const GhReleases = require('electron-gh-releases')
 
+//basic setup for GhReleases
+let options = {
+  repo: 'SamuelDub/DiscordBot-App',
+  currentVersion: app.getVersion()
+}
+
+const updater = new GhReleases(options)
 
 let passwordWindow;
 
@@ -21,16 +29,42 @@ app.on('ready', function () {
 
   const page = passwordWindow.webContents;
 
+  // const template = [{
+  //   label: 'File',
+  //   submenu: [{
+  //     label: 'Check For Updates',
+  //     click: function () {
+  //       updater.check((err, status) => {
+  //         if (!err && status) {
+  //           dialog.showMessageBox({
+  //             type: 'question',
+  //             buttons: ['Yes', 'No'],
+  //             title: 'Confirm',
+  //             message: 'A new update is available, would you like to download it and restart?'
+  //           }, function (response) {
+  //             if (response === 0) { // Runs the following if 'Yes' is clicked
+  //               downloadUpdate;
+  //             } else {
+  //               dialog.showMessageBox({
+  //                 type: 'info',
+  //                 buttons: ["Ok"],
+  //                 title: 'No update found',
+  //                 message: 'No new update was found'
+  //               })
+  //             }
+  //           })
+  //         }
+  //       })
+  //     }
+  //   }]
+  // }]
+
+  // const menu = Menu.buildFromTemplate(template)
+  // Menu.setApplicationMenu(menu)
+
   //waits for page to load before checking for updates
   page.once('did-frame-finish-load', () => {
 
-    let options = {
-      repo: 'SamuelDub/DiscordBot-App',
-      currentVersion: app.getVersion()
-    }
-
-    const updater = new GhReleases(options)
-    
     // Check for updates
     // `status` returns true if there is a new update available
     updater.check((err, status) => {
